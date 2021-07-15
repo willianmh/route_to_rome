cf9bad2da153614012c7ac462466e6280b55c236
Willian Hayashida <w188705@dmz.org.br>
2021-07-15 13:58:35 -0300

    deleted scripts

--- download_data.ps1
+++
@@ -1,2 +0,0 @@
-mkdir data
-wget http://www.inf.ufpr.br/vri/databases/LIBRAS-HC-RGBDS-2011.tar -OutFile ./data/LIBRAS-HC-RGBDS-2011.tar
--- download_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir data
-cd data
-wget http://www.inf.ufpr.br/vri/databases/LIBRAS-HC-RGBDS-2011.tar 
