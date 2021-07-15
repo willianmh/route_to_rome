038e9953bb3b51c1dfb6688de04fd542ccadf2f9
Willian Hayashida <w188705@dmz.org.br>
2021-07-15 12:10:44 -0300

    scripts

---
+++ download_data.ps1
@@ -0,0 +1,2 @@
+mkdir data
+wget http://www.inf.ufpr.br/vri/databases/LIBRAS-HC-RGBDS-2011.tar -OutFile ./data/LIBRAS-HC-RGBDS-2011.tar
---
+++ download_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir data
+cd data
+wget http://www.inf.ufpr.br/vri/databases/LIBRAS-HC-RGBDS-2011.tar 
